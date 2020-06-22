d9bb18deb92e9e7a893dcbfdfdedbba2d2f4b2d8
Emmanuel Pereira Pires <30904339+epcgrs@users.noreply.github.com>
2020-06-22 13:39:58 -0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,4 +8,11 @@ mix.js('resources/js/app.js', 'assets/js');
 
 mix.sass('resources/sass/app.scss', 'assets/css');
 
-mix.browserSync('http://wordpress.dev.com/');
+mix.browserSync({
+    proxy: 'http://yourproxy.dev.com',
+    files: [
+        './**/*.php',
+        './assets/js/*.js',
+        './assets/css/*.css',
+    ]
+});
