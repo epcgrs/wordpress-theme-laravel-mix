44082c0c4de2dc02462181686ef7015e034d4843
Emmanuel Pereira Pires <30904339+epcgrs@users.noreply.github.com>
2020-07-28 11:05:02 -0300

    disable processCssUrls

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,10 @@ mix.options({
 
 mix.js('resources/js/app.js', 'assets/js');
 
-mix.sass('resources/sass/app.scss', 'assets/css');
+mix.sass('resources/sass/app.scss', 'assets/css')
+.options({
+    processCssUrls: false
+});
 
 mix.browserSync({
     proxy: 'http://yourproxy.dev.com',
