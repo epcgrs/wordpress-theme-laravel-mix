ad7f0d6d0cc112c71b181119105c22e510ec7012
Emmanuel Pereira Pires <30904339+epcgrs@users.noreply.github.com>
2020-08-26 09:38:47 -0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,14 @@
 require('./bootstrap');
 
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+($ => {
+    $(() => {
+        // Jquery Functions
+    });
+})(jQuery);
+
 import Vue from 'vue';
 
 Vue.component('example', require('./components/Example.vue').default);
